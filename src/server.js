992612951e43f0dbfba80f78a09079e84754431b
huhn511 <sehe89@gmail.com>
2021-03-14 23:25:54 +0100

    fix name

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		dev ? '/' : 'website',
+		dev ? '/' : 'x-Team_IOTA_Identity',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
