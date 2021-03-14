5a9d160e4cd7e66ffeb9f1a1fef695c3ca06a692
huhn511 <sehe89@gmail.com>
2021-03-14 23:47:03 +0100

    add and deploy storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,24 +13,24 @@ module.exports = {
 		// `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
 		// You can change the configuration based on that.
 		// 'PRODUCTION' is used when building the static version of storybook.
-        let copy =  new CopyWebPlugin({
-            patterns: [
-                {
-                  from: 'node_modules/iota-identity-wasm-test/web/iota_identity_wasm_bg.wasm',
-                  to: 'iota_identity_wasm_bg.wasm'
-                }
-            ]
-        })
-        config.plugins.push(copy)
-		// Make whatever fine-grained changes you need
-		const loaderIndex = config.module.rules.findIndex(
-			(val) => val.loader && val.loader.includes('svelte-loader')
-		);
-		config.module.rules[loaderIndex].options = {
-			preprocess: require('svelte-preprocess')(
-				require('../svelte-preprocess.config').sveltePreprocessConfig
-			),
-		};
+        // let copy =  new CopyWebPlugin({
+        //     patterns: [
+        //         {
+        //           from: 'node_modules/iota-identity-wasm-test/web/iota_identity_wasm_bg.wasm',
+        //           to: 'iota_identity_wasm_bg.wasm'
+        //         }
+        //     ]
+        // })
+        // config.plugins.push(copy)
+		// // Make whatever fine-grained changes you need
+		// const loaderIndex = config.module.rules.findIndex(
+		// 	(val) => val.loader && val.loader.includes('svelte-loader')
+		// );
+		// config.module.rules[loaderIndex].options = {
+		// 	// preprocess: require('svelte-preprocess')(
+		// 	// 	require('../svelte-preprocess.config').sveltePreprocessConfig
+		// 	// ),
+		// };
 
   
 
